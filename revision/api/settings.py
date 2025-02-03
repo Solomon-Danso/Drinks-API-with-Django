8494511b344b8cb67f37490365d5fcca611619bc
Solomon Danso <solomondanso2023@gmail.com>
2025-02-03 06:39:37 +0000

    Update

--- revision/api/settings.py
+++ revision/api/settings.py
@@ -71,7 +71,9 @@ TEMPLATES = [
 REST_FRAMEWORK = {
         'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 WSGI_APPLICATION = 'api.wsgi.application'
